30afdf976f1cb4c84615645ac9c0e0a05e6e2af8
Emirhan Yağcı <emirhan.yac2@gmail.com>
2023-08-21 16:51:39 +0300

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
   Settings,
   Users,
 } from "./pages";
+import AppLayout from "./ui/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
 function App() {
   return (
@@ -16,16 +17,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route
-            index
-            element={<Navigate replace={true} to="dashboard" />}
-          ></Route>
-          <Route path="dashboard" element={<Dashboard />}></Route>
-          <Route path="Bookings" element={<Bookings />}></Route>
-          <Route path="Cabins" element={<Cabins />}></Route>
-          <Route path="Users" element={<Users />}></Route>
-          <Route path="Settings" element={<Settings />}></Route>
-          <Route path="Account" element={<Account />}></Route>
+          <Route element={<AppLayout />}>
+            <Route
+              index
+              element={<Navigate replace={true} to="dashboard" />}
+            ></Route>
+            <Route path="dashboard" element={<Dashboard />}></Route>
+            <Route path="Bookings" element={<Bookings />}></Route>
+            <Route path="Cabins" element={<Cabins />}></Route>
+            <Route path="Users" element={<Users />}></Route>
+            <Route path="Settings" element={<Settings />}></Route>
+            <Route path="Account" element={<Account />}></Route>
+          </Route>
           <Route path="Login" element={<Login />}></Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
