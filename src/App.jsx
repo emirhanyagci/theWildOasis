885954ce772f2904bf6b37bd8bf04b92a9c03d31
Emirhan Yağcı <emirhan.yac2@gmail.com>
2023-08-21 11:36:50 +0300

    global style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/globalStyles.js";
+const H1 = styled.h1`
+  color: red;
+`;
+
 function App() {
-  return <div>Hello new App</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <H1>Hello new App</H1>
+    </div>
+  );
 }
 
 export default App;
