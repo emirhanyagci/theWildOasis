5bf8d5d58e480ef8fd79610a062ec560fa19dc3f
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-24 10:21:51 +0300

    api side pagination done

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -5,8 +5,10 @@ import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, count, isLoading } = useBookings();
+  console.log(bookings);
   if (isLoading) return <Spinner />;
+  if (bookings.length <= 0) return null;
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
@@ -26,7 +28,7 @@ function BookingTable() {
           )}
         />
         <Table.Footer>
-          <Pagination count={11} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -14,13 +14,15 @@ export function useBookings() {
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
   const {
-    data: bookings,
+    data: { data: bookings, count } = {},
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings(filter, sortBy),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings(filter, sortBy, page),
   });
-  return { bookings, isLoading, error };
+  return { bookings, count, isLoading, error };
 }
