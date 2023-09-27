d0741be60224400edfeca369cfab7c0151767798
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-27 12:00:21 +0300

    login page and logun function builded

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
-
+import { useLogin } from "./useLogin";
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleSubmit() {}
+  const [email, setEmail] = useState("emirhan.yacis@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const { login, isLoading } = useLogin();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +25,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -29,10 +35,11 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large">{!isLoading ? "Login" : <SpinnerMini />} </Button>
       </FormRowVertical>
     </Form>
   );
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -18,6 +18,13 @@ const Header = styled.h1`
       font-size: 2rem;
       font-weight: 5 00;
     `}
+    ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+      text-align: center;
+    `}
 `;
 
 export default Header;
