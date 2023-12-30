be58ab1ec9115d30d6ae94994286c474704412b6
Emirhan Yagci <emirhan.yac2@gmail.com>
2023-12-30 18:53:20 +0300

    today activity added to dasboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -28,7 +29,7 @@ function DashboardLayout() {
       >
         Stats
       </Stats>
-      <div>Todays Activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
